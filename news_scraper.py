7012867959711933822b3131d5fe626cf229fa10
Michael J Wolfe <michael.wolfe2@ascension.org>
2020-04-25 10:40:48 -0500

    added efficient scraper, data

--- news_scraper.py
+++ news_scraper.py
@@ -1,14 +1,29 @@
 from newsapi import NewsApiClient
 from operator import itemgetter
+import csv
 
 newsapi = NewsApiClient(api_key="ee1bf85ed47f48dd8f89da2c29c7b02b")
 
-sources = newsapi.get_sources(country='us')
-source_list = list(map(itemgetter('name'), sources['sources']))
+#populates source list, don't run unless needed
+#sources = newsapi.get_sources(country='us')
+#source_list = list(map(itemgetter('name'), sources['sources']))
 
-headlines = newsapi.get_top_headlines(q='covid',
-                                      language='en',
-                                      country='us')
+#with open('data/source_list.csv','w') as result_file:
+#    wr = csv.writer(result_file, dialect='excel')
+#    wr.writerows(source_list)
+
+#not needed - save some API calls
+#headlines = newsapi.get_top_headlines(q='covid',
+#                                      language='en',
+#                                      country='us')
+
+with open ('data/source_list.csv', newline='') as f:
+    reader = csv.reader(f)
+    read_list = list(reader)
+
+source_list = []
+for string in read_list:
+    source_list.append(''.join(string))
 
 article_list, content_list = ([], ) * 2
 for source in source_list:
@@ -19,11 +34,11 @@ for source in source_list:
                                          from_param='2020-04-01',
                                          to='2020-04-10',
                                          sort_by='relevancy')
-        article_list.append(str(map(itemgetter('title'), all['articles'])))
-        content_list.append(str(map(itemgetter('content'), all['articles'])))
+        #article_list.append(str(map(itemgetter('title'), all['articles'])))
+        #content_list.append(str(map(itemgetter('content'), all['articles'])))
     except:
         print(source+' unavailable in selected range')
 
-article_dict = {article_list[i]: content_list[i] for i in range(len(article_list))}
-
-print(article_dict)
+with open('data/article_data.csv','w') as data_file:
+    wr = csv.writer(data_file)
+    wr.writerows(all['articles'])
