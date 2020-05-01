75b837db5d0940e6fd887e999b797ba36d7f39ca
Michael J Wolfe <michael.wolfe2@ascension.org>
2020-04-30 21:45:52 -0500

    switched to tweepy for functional API

--- twitter_scraper.py
+++ twitter_scraper.py
@@ -1,12 +1,10 @@
-import twitter
+import tweepy
 
-api = twitter.Api(consumer_key='coming soon...',
-                  consumer_secret='coming soon...',
-                  access_token_key='coming soon...',
-                  access_token_secret='coming soon...',
-                  sleep_on_rate_limit=True)
+auth = tweepy.OAuthHandler(consumer_key='get from David', consumer_secret='get from David')
+auth.set_access_token(key='get from David', secret='get from David')
 
-raw_search = api.GetSearch(
-    raw_query="q=(covid%20OR%20coronavirus)%20since%3A2020-03-15")
+api = tweepy.API(auth)
+
+raw_search = api.search(q="(covid%20OR%20coronavirus)%20since%3A2020-03-15")
 
 print(raw_search)
